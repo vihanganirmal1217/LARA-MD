9b7161f08c3281735152c53f49bd9f17a8ead924
LARA-MD2 <cmodzsadaruwan@gmail.com>
2025-02-14 22:16:04 +0530

    Update menu.js

--- lara/plugins/menu.js
+++ lara/plugins/menu.js
@@ -105,7 +105,7 @@ let cap = `
                       serverMessageId: 143
                     }
                     */
-                    }
+                    //}
               })
             }
           }
