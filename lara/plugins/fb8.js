91685503085691a1f5fa0811b284ee7c7194fc41
LARA-MD2 <cmodzsadaruwan@gmail.com>
2025-02-14 10:46:25 +0530

    Update fb8.js

--- lara/plugins/fb8.js
+++ lara/plugins/fb8.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 //const fg = require("api-dylux");
 //const xnxx = require("xnxx-dl");
 const yts = require("yt-search");
-const config = require("../settings");
+const config = require('../../settings')
 const {
   File
 } = require('megajs');
